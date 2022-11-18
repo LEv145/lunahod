c72bbcd55b646b9a9b6c617534a2603e5023da5e
LEv145 <you@example.com>
2022-11-18 16:57:55 +0200

    v 0.1
    Update structure
    Add `__init__.py`
    Add `.gitignore`

---
+++ src/lunohod/__init__.py
@@ -0,0 +1,5 @@
+from lunohod.common import AngularServo360, AngularServo90, AngularServo90Plus
+from lunohod.impls.wheels import Wheels
+from lunohod.impls.bucket import Bucket
+from lunohod.impls.claw import Claw
+from lunohod.impls.hand import Hand
