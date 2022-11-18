9818ded7542ac9932c98d2738715f7c0878663c3
LEv145 <you@example.com>
2022-11-18 22:02:36 +0200

    v 0.1
    Update tests

--- tests/wheels.py
+++
@@ -1,9 +0,0 @@
-from lunohod.wheels import Wheels, VNH3SP30Motor, AngularServo90
-
-
-wheels = Wheels(
-    motor=VNH3SP30Motor(IN_A=16, IN_B=20, PWM=21, EN=12),
-    servo=AngularServo90(7),
-)
-
-breakpoint()
